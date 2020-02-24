d58c7273f77703cb3153c3fdc362be9c8c7847a2
Alexandre Fukaya <afukaya@hotmail.com>
2020-02-24 20:14:23 -0300

    Ash Cloud Simulator Version 1

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# ashclouds
+﻿# Problema B - Nuvem de Cinzas
+
+É o caos nos aeroportos! Um vulcão acaba de entrar em erupção provocando uma nuvem de cinzas que se alastra e impede a circulação aérea. O governo da Onilândia está muito preocupado e quer saber quando é que a nuvem de cinzas irá atingir os aeroportos onilandeses.
+
+O governo tem acesso a um mapa obtido via satélite que detalha a situação corrente. O mapa é um rectângulo que está dividido em quadrículas mais pequenas. Tendo em conta a situação em análise, apenas são distinguidos três tipos de quadrículas: nuvem (indicando que esse sector do mapa está neste momento coberto por uma nuvem de cinzas), aeroporto (letra 'A', indicando que esse sector do mapa contém um aeroporto) e todas as outras (que não têm neste momento nem uma nuvem nem um aeroporto). Um exemplo de um mapa seria o indicado na figura seguinte:
+
+À medida que o tempo vai passando a situação vai piorando. De facto, por cada dia que passa, a nuvem expande-se uma quadrícula na horizontal e na vertical. Dito de outro modo, ao fim de um dia, todas as quadrículas que estavam adjacentes (vertical ou horizontalmente) a uma quadrícula com nuvem, passam também elas a conter nuvens. Exemplificando a evolução da situação ao fim de dois dias, teríamos o seguinte:
+
+```
+Hoje                 Amanhã                 2 dias depois
+. . * . . . * *      . * * * . * * *        * * * * * * * *
+. * * . . . . .      * * * * . . * *        * * * * * * * *
+* * * . A . . A      * * * * A . . A        * * * * * . * *
+. * . . . . . .  ->  * * * . . . . .   ->   * * * * . . . .
+. * . . . . A .      * * * . . . A .        * * * * . . A .
+. . . A . . . .      . * . A . . . .        * * * A . . . .
+. . . . . . . .      . . . . . . . .        . * . . . . . .
+```
+
+Para preparar convenientemente os planos de contingência, o governo necessita de saber duas coisas: quantos dias demorará até pelo menos um aeroporto ficar coberto pela nuvem e daqui a quantos dias os aeroportos estarão todos eles cobertos pela nuvem. Tens de ajudar!
+
+## O Problema
+
+Dado um quadriculado de L linhas por C colunas indicando a posição actual da nuvem e dos aeroportos, a tua tarefa é descobrir Nmin, o número de dias até um primeiro aeroporto ficar debaixo da nuvem de cinzas e Nmax, o número de dias até todos os aeroportos ficarem cobertos pelas cinzas.
+
+## Input
+
+Na primeira linha do input vem dois números inteiros L e C, separados por um espaço, indicando respectivamente o número de linhas e o número de colunas do mapa.
+
+Seguem-se exactamente L linhas, cada uma contendo exactamente C caracteres, descrevendo o mapa. Cada um dos caracteres pode ser:
+
+    '*', indicando que a quadrícula tem presentemente uma nuvem
+    'A', indicando que a quadrícula tem um aeroporto
+    '.', indicando que a quadrícula não tem neste momento uma nuvem nem um aeroporto
+
+Existe sempre pelo menos uma quadrícula com nuvem e uma quadrícula com um aeroporto, mas não deves assumir à partida mais nada sobre as outras quadrículas.
+Output
+
+O output deve ser constituído exactamente por uma única linha contendo dois números inteiros Nmin e Nmax, separados por um único espaço, indicando respectivamente o número de dias até que um primeiro aeroporto fique coberto pela nuvem e o número de dias até que todos os aeroportos fiquem cobertos.
+Restrições
+
+São garantidos os seguintes limites em todos os casos de teste que irão ser colocados ao programa:
+
+1 ≤ L,C ≤ 1000 Dimensões do mapa
+
+## Nota sobre a avaliação
+
+Para um conjunto de casos de teste valendo 50% dos pontos, o número de linhas e de colunas é inferior a 50.
+
+## Exemplo de Input
+
+```
+7 8
+..#...##
+.##.....
+###.A..A
+.#......
+.#....A.
+...A....
+........
+```
+
+## Exemplo de Output
+```
+2 4
+```
+
+O problema acima foi extraido de https://www.dcc.fc.up.pt/oni/problemas/2010/qualificacao/probB.html
---
+++ ashclouds.py
@@ -0,0 +1,130 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+NAME  : ashclouds.py
+AUTHOR: Alexandre Fukaya
+DATE  : 23/02/2020
+
+DESCRIPTION:
+    Resolução do problema para o espalhamento de nuvens de cizas vulcânicas 
+    proposto em https://www.dcc.fc.up.pt/oni/problemas/2010/qualificacao/probB.html
+    
+DEPENDENCIES:
+
+TODO:
+    
+"""
+import sys
+import os
+import copy
+
+def load_data(file_path, file_name):
+    data      = list()
+    data_file = file_path + file_name
+
+    with open(data_file) as f:
+        for line in f:
+            data.append(line)
+    return data
+
+def transform_data(data):
+    size_info = data[0].strip('\n').split(' ')
+    lin = int(size_info[0])
+    col = int(size_info[1])
+    data_array = []
+    airports = 0
+
+    for i in range(1,lin+1):
+        data_line = data[i].upper().strip('\n')
+        airports = airports + data[i].count('A')
+        data_array.append(list(data_line))
+    return airports, lin, col, data_array
+
+def simulate_clouds(airports, max_lines, max_cols, data):
+    airports_affected = 0
+    day               = 0
+    end_simulation    = False
+    max_data          = 0
+    min_data          = 0
+
+    today_data = copy.deepcopy(data)
+
+    print('Vulcanic Ash Clouds Simulator')
+    print()
+
+    while(not end_simulation):
+        # For each pass clone the matix and make the changes at the copy.
+        # Store the copy for output and calculations.
+        tomorrow_data = copy.deepcopy(today_data)
+        lin = 0
+
+        # Print the status for current the day
+        print('Day {0}'.format(day))
+        print('Airports affected {0}'.format(airports_affected))
+        print('Current Status')
+        for line in today_data: print(line)
+        print()
+
+        # Calculates the next day status.
+        for data_line in today_data:
+            col = 0
+            for info in data_line:
+                if info == '#': # found a ash cloud, need to check boundaries
+                    neg_col = col - 1
+                    neg_lin = lin - 1
+                    pos_col = col + 1
+                    pos_lin = lin + 1
+
+                    # Check coordinates for out of boudaries values.
+                    if neg_col < 0 : neg_col = 0
+                    if neg_lin < 0 : neg_lin = 0
+                    if pos_col > max_cols  - 1 : pos_col = max_cols  - 1
+                    if pos_lin > max_lines - 1 : pos_lin = max_lines - 1
+
+                    # Cover neighborhoods with ashes
+                    tomorrow_data[lin][neg_col] = '#'
+                    tomorrow_data[lin][pos_col] = '#'
+                    tomorrow_data[neg_lin][col] = '#'
+                    tomorrow_data[pos_lin][col] = '#'
+
+                col = col + 1
+            lin = lin + 1
+
+        # Check for affected airports
+        for today_line,tomorrow_line in zip(today_data,tomorrow_data):
+            for today_value,tomorrow_value in zip(today_line,tomorrow_line):
+                if (today_value == 'A') and (today_value != tomorrow_value):
+                    if (airports_affected == 0) : min_data = day + 1
+
+                    airports_affected += 1
+
+                    if (airports_affected == airports) : 
+                        max_data = day + 1 
+                        end_simulation = True
+
+        today_data = tomorrow_data
+        day = day + 1
+
+    # Print last day status
+    print('Day {0}'.format(day))
+    print('Airports affected {0}'.format(airports_affected))
+    print('Current Status')
+    for line in today_data: print(line)
+    print()
+
+    # Print simmulation summary
+    print('Simularion ended')
+    print('Days before the first airport is reached {0}'.format(min_data))
+    print('Days before all airports are affected {0}'.format(max_data))
+    return 
+
+def main():
+    file_path = 'D:\\Users\\afukaya\\OneDrive\\Repos\\Github\\Organizations\\DojoPuzzle\\ashclouds\\'
+    file_name = 'ash1.txt'
+    raw_data = load_data(file_path,file_name)
+    airport_count, max_lines, max_cols, ash_data = transform_data(raw_data)
+    simulate_clouds(airport_count, max_lines, max_cols, ash_data)
+
+if __name__ == "__main__":
+    main()
